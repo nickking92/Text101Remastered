a112de5fc2621525c70b9df71a872aa3b7c498f3
Nikola <nickking92@gmail.com>
2019-09-12 17:02:13 +0200

    TEXT101

---
+++ Assets/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField] Text TextComponent;
+    [SerializeField] State startingState;
+
+     
+
+    State state;
+    // Use this for initialization
+ 
+    void Start()
+    {
+        state = startingState;
+        TextComponent.text = state.GetStateStory();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        ManageState();
+
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+
+            }
+        }
+        TextComponent.text = state.GetStateStory();
+    }
+}
---
+++ Assets/State.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(menuName ="State")]
+
+
+public class State : ScriptableObject {
+    [TextArea(10, 44)]
+    [SerializeField] string storyText;
+    [SerializeField] State []nextStates;
+    public string GetStateStory()
+    {
+          return storyText;
+    }
+    public State[] GetNextStates()
+    {
+    
+    
+        return nextStates;
+    }
+}
